c56e405d99183b6eea8bf6fc456cda5f0fbdff6a
Yassin <yassineldeeb94@gmail.com>
2021-08-02 22:12:08 +0200

    try removing the set cookie

--- src/auth/sendRefreshToken.ts
+++ src/auth/sendRefreshToken.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express'
 
 export const sendRefreshToken = (res: Response, refreshToken: string) => {
-  res.cookie('jid', refreshToken, {
-    httpOnly: true,
-    path: '/refresh_token',
-    secure: process.env.SERVER_URL!.includes('https') ? true : false,
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-  })
+  // res.cookie('jid', refreshToken, {
+  //   httpOnly: true,
+  //   path: '/refresh_token',
+  //   secure: process.env.SERVER_URL!.includes('https') ? true : false,
+  //   maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  // })
 }
