34f8272b46fd87be886aaa204725afb89c0f76cf
Yassin <yassineldeeb94@gmail.com>
2021-08-03 00:05:17 +0200

    why github oauth doesn't work in production

--- src/OAuth/Github/strategy.ts
+++ src/OAuth/Github/strategy.ts
@@ -4,12 +4,16 @@ import { Strategy as GitHubStrategy } from 'passport-github2'
 
 const githubStrategyRouter = express.Router()
 
+const env = (envVar: string) => {
+  return envVar.replace('\n', '')
+}
+
 passport.use(
   new GitHubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID!,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      callbackURL: `${process.env.SERVER_URL!}/auth/github/callback`,
+      clientID: env(process.env.GITHUB_CLIENT_ID!),
+      clientSecret: env(process.env.GITHUB_CLIENT_SECRET!),
+      callbackURL: `${env(process.env.SERVER_URL!)}/auth/github/callback`,
       scope: ['user:email'],
     },
     async (accessToken: any, refreshToken: any, profile: any, done: any) => {
@@ -20,10 +24,3 @@ passport.use(
 )
 
 export { githubStrategyRouter }
-
-console.log({
-  clientID: process.env.GITHUB_CLIENT_ID!,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-  callbackURL: `${process.env.SERVER_URL!}/auth/github/callback`,
-  scope: ['user:email'],
-})
