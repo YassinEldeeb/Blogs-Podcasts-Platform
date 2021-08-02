dbbb05caeae9981b1da0d70252809f61c600138a
Yassin <yassineldeeb94@gmail.com>
2021-08-02 06:58:33 +0200

    debugging...

--- src/auth/sendRefreshToken.ts
+++ src/auth/sendRefreshToken.ts
@@ -4,6 +4,7 @@ export const sendRefreshToken = (res: Response, refreshToken: string) => {
   res.cookie('jid', refreshToken, {
     httpOnly: true,
     path: '/refresh_token',
-    secure: process.env.SERVER_URL!.includes('https') ? true : false,
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
   })
 }
