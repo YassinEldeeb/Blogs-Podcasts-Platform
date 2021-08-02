c13ae1921ab621d6f88a4ddc34cd6cece8239fdc
Yassin <yassineldeeb94@gmail.com>
2021-08-03 00:49:11 +0200

    changeing cors

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,7 @@ const pubsub = new RedisPubSub({ connection: { host: process.env.REDIS_HOST } })
 
   app.use(
     cors({
-      origin: ['http://localhost:3000'],
+      origin: ['http://localhost:3000', 'http://127.0.0.1:5500'],
       credentials: true,
     })
   )
