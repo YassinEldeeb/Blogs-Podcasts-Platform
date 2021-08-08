ecd9a67d0057c8dd2fea66a6316e2bbaa8e5d1be
Yassin <yassineldeeb94@gmail.com>
2021-08-08 11:33:16 +0200

    trying without cron

--- backup/src/app.ts
+++ backup/src/app.ts
@@ -29,26 +29,22 @@ async function saveBackupOnS3(filePath: string) {
 }
 
 async function startBackupSchedule() {
-  cron.schedule(
-    '30 9 * * *',
-    async () => {
-      genDate()
-      const outputPath = path.join(__dirname, '../target.zip')
-      const output = fs.createWriteStream(outputPath)
-      const archive = archiver('zip')
+  // cron.schedule('30 9 * * *', async () => {
+  genDate()
+  const outputPath = path.join(__dirname, '../target.zip')
+  const output = fs.createWriteStream(outputPath)
+  const archive = archiver('zip')
 
-      archive.pipe(output)
+  archive.pipe(output)
 
-      archive.file(path.join(__dirname, '../backups/backup-now.sql'), {
-        name: `backup-${currentDate}.sql`,
-      })
+  archive.file(path.join(__dirname, '../backups/backup-now.sql'), {
+    name: `backup-${currentDate}.sql`,
+  })
 
-      await archive.finalize()
+  await archive.finalize()
 
-      saveBackupOnS3(outputPath)
-    },
-    {}
-  )
+  saveBackupOnS3(outputPath)
+  // })
 }
 
 startBackupSchedule()
