23a6627636241b90877af0543815226731ee3f29
Yassin <yassineldeeb94@gmail.com>
2021-08-09 14:58:47 +0200

    testing backup

--- packages/backup/src/app.ts
+++ packages/backup/src/app.ts
@@ -25,7 +25,7 @@ async function saveBackupOnS3(filePath: string) {
 }
 
 async function startBackupSchedule() {
-  cron.schedule('55 12 * * *', async () => {
+  cron.schedule('3 13 * * *', async () => {
     genDate()
 
     const output = fs.createWriteStream(zipOutputPath)
